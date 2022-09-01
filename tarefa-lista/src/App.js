aca5cafb60a6fbfd7af7533f73d2bb8a88bff4fc
WilliamSchreiner <williamg.schreiner@gmail.com>
2022-09-01 17:03:00 -0300

    Apenas falta a funçao de Editar e de marcar como feita

--- tarefa-lista/src/App.js
+++ tarefa-lista/src/App.js
@@ -1,41 +1,78 @@
-import { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-
+//envio de forma para nao dar erro nas variaveis
+import React, { useState, useRef} from "react";
+//O useRef nos permite persistir valores entre renderizações, 
+//ele pode ser usado para armazenar o valor de uma variável mutável
 
 function App() {
+
+  // States definidos.
   const [tarefa, setTarefa] = useState();
-  const [tarefas, setTarefas] = useState([]);
+  const [historico, setHistorico] = useState([]);
+  const [listaTarefas, setListaTarefas] = useState([]);
 
+  // REf ID definido.
+  const idTarefa = useRef(0);
 
-  function adicionar() {
-    console.log('adicionar ')
-    tarefas.push(tarefa);
-    setTarefas([...tarefas]);
+  //Functions
+  function salvar(){
+    setListaTarefas(old => {return [...old, {id: idTarefa.current, texto: tarefa} ]})
+    idTarefa.current++;
+    setTarefa(' ')
+  }
+  function limparHistorico(){
+    setHistorico( [ ] )
+    idTarefa = 0;
+  }
+  function concluir(id){
+    // Terminar
 
-    console.log('tarefas ', tarefas)
+  
+  }
+  function deleteTarefa(id){
+  const tmp = listaTarefas.filter(tarefa => tarefa.id !== id );
+  // o TMP recebera todas as tarefas cujo ID seja diferente da que ele recebeu.
+  setListaTarefas(tmp);
+  }
+  function editarTarefa(id){
+    //Terminar
   }
 
-  return (
-    <div className="container">
-      <h1>Aluno - Notas</h1>
-      <form>
-        <div className="mb-3">
-          <label className="form-label">sua tarefa</label>
-          <input type="text" className="form-control" onChange={(event) => setTarefas(event.target.value)} />
-        </div>
+return (
+<>
+  <h3> listas de tarefas</h3>
 
-        <button type="button" onClick={adicionar}>Adicionar</button>
-      </form>
-
-      <ul>
-        {
-          tarefas.map((n, index) => {
-            return <li key={index}>{n}</li>
-          })
-        }
-      </ul>
+  <form>
+    <div id="form">
+    <input  type="text" value={tarefa} onChange={(evento) => {setTarefa(evento.target.value) }}></input>
+    <br/>
+    <button type="button" className="botao" onClick={salvar}>Salvar</button>
+    <button type="button" className="botao" onClick={limparHistorico}>Limpar Historico</button>
     </div>
-  )
+  </form>
+
+  <div className="lista-tarefas">  
+  <label>Suas tarefas:</label>
+  {listaTarefas.map((tarefa) => {
+    return  <p key={tarefa.id} className="p-tarefa">
+      {tarefa.texto} 
+      <input type="checkbox" onClick={() => concluir(tarefa.id)}></input>
+      <button id="botao-delete" onClick={() => deleteTarefa(tarefa.id)}>delete</button>
+      <button id="botao-editar" onClick={() => editarTarefa(tarefa.id)}>editar</button>
+    </p> 
+  })}
+  </div>
+
+  <div className="lista-tarefas">  
+  <label>Suas tarefas concluidas:</label>
+  {historico.map((tarefa) => {
+    return  <p key={tarefa.id} id="tarefa-feita" className="p-tarefa">
+      {tarefa.texto}</p> 
+  })}
+  </div> 
+</>
+);
 }
 
 export default App;
