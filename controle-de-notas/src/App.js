406461aeb5e2246a1f1d2ea5930a6c59520d2995
WilliamSchreiner <williamg.schreiner@gmail.com>
2022-09-02 17:40:28 -0300

    adicionei o footer

--- controle-de-notas/src/App.js
+++ controle-de-notas/src/App.js
@@ -116,6 +116,14 @@ function App() {
           }
         </tbody>
       </table>
+
+      <footer>
+      <h4>Integrantes:</h4>
+      <p> 1° Amilto;</p>
+      <p> 2° João Pedro;</p>
+      <p> 3° Lucas Edu. Faleiro;</p>
+      <p> 4° William.</p>
+    </footer>
     </div>
 
   );
